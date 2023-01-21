7022702231cd95ccd1f2b9fab54766314f3d32d6
Fredy <44968046+Liontek1985@users.noreply.github.com>
2023-01-21 20:03:45 +0100

    working drastic for mali

--- scriptmodules/emulators/drastic.sh
+++ scriptmodules/emulators/drastic.sh
@@ -14,42 +14,16 @@ rp_module_desc="NDS emu - DraStic"
 rp_module_help="ROM Extensions: .nds .zip\n\nCopy your Nintendo DS roms to $romdir/nds"
 rp_module_licence="PROP"
 rp_module_section="exp"
-rp_module_flags="sdl2 !all arm !armv6 !mali"
+rp_module_flags=""
 
-function depends_drastic() {
-    local depends=(libasound2-dev libsdl2-dev zlib1g-dev)
-    if isPlatform "kms" && ! isPlatform "x11"; then
-        depends+=(matchbox-window-manager xorg xserver-xorg-input-all)
-    fi
-
-    getDepends ${depends[@]}
-}
-
-function __binary_url_drastic() {
-   echo "$__archive_url/drastic-2.5.0.4.tar.gz"
-}
 
 function install_bin_drastic() {
-    downloadAndExtract "$(__binary_url_drastic)" "$md_inst" --strip-components 1
+    downloadAndExtract "https://github.com/Liontek1985/mpcore-library/raw/nxt-legacy/emulators/drastic.tar.gz" "$md_inst" --strip-components 1
+    patchVendorGraphics "$md_inst/drastic"
 }
 
 function configure_drastic() {
     mkRomDir "nds"
-
-    addEmulator 1 "$md_id" "nds" "$md_inst/drastic.sh %ROM%"
-    addSystem "nds"
-
-    [[ "$md_mode" == "remove" ]] && return
-
-    isPlatform "kms" && ! isPlatform "x11" && setBackend "$md_id" "x11"
-
-    cat > "$md_inst/drastic.sh" << _EOF_
-#!/bin/bash
-pushd "$md_conf_root/nds/drastic"
-$md_inst/drastic "\$1"
-popd
-_EOF_
-    chmod +x "$md_inst/drastic.sh"
 
     # wrong permissions on game_database.xml
     chmod 644 "$md_inst/game_database.xml"
@@ -61,4 +35,7 @@ _EOF_
     for file in game_database.xml system/drastic_bios_arm7.bin system/drastic_bios_arm9.bin usrcheat.dat drastic_logo_0.raw drastic_logo_1.raw; do
         ln -sfv "$md_inst/$file" "$md_conf_root/nds/drastic/$file"
     done
+
+    addEmulator 1 "$md_id" "nds" "pushd $md_conf_root/nds/drastic; $md_inst/drastic %ROM%; popd"
+    addSystem "nds"
 }
