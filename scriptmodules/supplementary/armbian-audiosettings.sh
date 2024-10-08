7b3d29449db0e2f0cf8b4256bb0577827f2378ff
Arnaldo Valente <gleam2003@msn.com>
2024-10-08 10:51:31 +0200

    Add softvol for hdmi audio, set default/hdmi on emulationstation

--- scriptmodules/supplementary/armbian-audiosettings.sh
+++ scriptmodules/supplementary/armbian-audiosettings.sh
@@ -10,11 +10,11 @@
 #
 
 rp_module_id="armbian-audiosettings"
-rp_module_desc="Configure audio settings with dmix"
+rp_module_desc="Configure audio settings for Armbian"
 rp_module_section="config"
 rp_module_flags="!all armbian"
 
-function depends_audiosettings() {
+function depends_armbian-audiosettings() {
     if [[ "$md_mode" == "install" ]]; then
         getDepends alsa-utils
     fi
@@ -27,75 +27,26 @@ function gui_armbian-audiosettings() {
         return
     fi
 
-    # Auto-configure dmixer
-    _alsa_audiosettings $hw
+    _armbian_alsa_internal_audiosettings
 }
 
-function _autoconfigure_dmix_audiosettings() {
-    local hw=$1  # Pass the sound card hardware number
-
-    # Target config file (system-wide or user-specific)
-    local config_file="/etc/asound.conf"
-    [[ ! -w "$config_file" ]] && config_file="$home/.asoundrc"  # Use user config if system config is not writable
-
-    # Create the ALSA configuration for dmix
-    cat << EOF > "$config_file"
-pcm.!default {
-  type plug
-  slave.pcm "dmixer"
-}
-
-pcm.dmixer  {
-  type dmix
-  ipc_key 1024
-  slave {
-    pcm "hw:$hw,0"
-    channels 2          # Ensures stereo output
-    period_time 0
-    period_size 1024
-    buffer_size 4096
-    rate 48000
-  }
-  bindings {
-    0 0
-    1 1
-  }
-}
-
-ctl.dmixer {
-  type hw
-  card $hw
-}
-
-ctl.!default {
-    type hw
-    card $hw
-}
-EOF
-
-    # Apply the new ALSA configuration
-    alsactl store
-    printMsgs "dialog" "Audio output configured with dmix for sound card hw:$hw"
-}
-
-function _reset_alsa_audiosettings() {
+function _reset_alsa_armbian-audiosettings() {
     /etc/init.d/alsa-utils reset
     alsactl store
-    rm -f "$home/.asoundrc" "/etc/alsa/conf.d/99-audiosettings.conf"
+    rm -f "$home/.asoundrc" "/etc/alsa/conf.d/99-retropie.conf"
     printMsgs "dialog" "Audio settings reset to defaults"
 }
 
-function _alsa_audiosettings() {
+function _armbian_alsa_internal_audiosettings() {
     local cmd=(dialog --backtitle "$__backtitle" --menu "Set audio output (ALSA)" 22 86 16)
     local options=()
     local card_index
     local card_label
 
-    # Get the list of sound cards from ALSA
+    # Get the list of Pi internal cards
     while read card_no card_label; do
         options+=("$card_no" "$card_label")
-    done < <(aplay -ql | sed -En -e '/^card/ {s/^card ([0-9]+): ([^[]+).*/\1 \2/; s/hdmi[- ]?/HDMI /i; p}')
-    
+    done < <(aplay -ql | sed -En -e '/^card/ {s/^card ([0-9]+): [^[]* \[([^]]*)\].*/\1 \2/; p}')
     options+=(
         M "Mixer - adjust output volume"
         R "Reset to default"
@@ -105,7 +56,7 @@ function _alsa_audiosettings() {
     if [[ -n "$choice" ]]; then
         case "$choice" in
             [0-9])
-                _autoconfigure_dmix_audiosettings $choice
+                _asoundrc_save_armbian-audiosettings $choice ${options[$((choice*2+1))]}
                 printMsgs "dialog" "Set audio output to ${options[$((choice*2+1))]}"
                 ;;
             M)
@@ -113,8 +64,78 @@ function _alsa_audiosettings() {
                 alsactl store
                 ;;
             R)
-                _reset_alsa_audiosettings
+                _reset_alsa_armbian-audiosettings
                 ;;
         esac
     fi
+}
+
+# configure the default ALSA soundcard based on chosen card index and type
+function _asoundrc_save_armbian-audiosettings() {
+    [[ -z "$1" ]] && return
+
+    local card_index=$1
+    local card_type=$2
+    local tmpfile="$(mktemp)"
+
+    if isPlatform "kms" && ! isPlatform "dispmanx" && [[ $card_type == "HDMI"* ]] || [[ $card_type == "hdmi"* ]]; then
+        # when the 'vc4hdmi' driver is used instead of 'bcm2835_audio' for HDMI,
+        # the 'hdmi:vchdmi[-idx]' PCM should be used for converting to the native IEC958 codec
+        # adds a volume control since the default configured mixer doesn't work
+        # (default configuration is at /usr/share/alsa/cards/vc4-hdmi.conf)
+        local card_name="$(cat /proc/asound/card${card_index}/id)"
+        cat << EOF > "$tmpfile"
+pcm.hdmi${card_index} {
+  type asym
+  playback.pcm {
+    type plug
+    slave.pcm "hw:$card_index,0"
+  }
+}
+ctl.!default {
+  type hw
+  card $card_index
+}
+pcm.softvolume {
+    type           softvol
+    slave.pcm      "hdmi${card_index}"
+    control.name  "HDMI Playback Volume"
+    control.card  ${card_index}
+}
+
+pcm.softmute {
+    type softvol
+    slave.pcm "softvolume"
+    control.name "HDMI Playback Switch"
+    control.card ${card_index}
+    resolution 2
+}
+
+pcm.!default {
+    type plug
+    slave.pcm "softmute"
+}
+EOF
+    else
+    cat << EOF > "$tmpfile"
+pcm.!default {
+  type asym
+  playback.pcm {
+    type plug
+    slave.pcm "output"
+  }
+}
+pcm.output {
+  type hw
+  card $card_index
+}
+ctl.!default {
+  type hw
+  card $card_index
+}
+EOF
+    fi
+    local dest="$home/.asoundrc"
+    mv "$tmpfile" "$dest"
+    chmod 644 "$dest"
 }
