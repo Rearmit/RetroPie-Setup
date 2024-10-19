e213c575d3e7cedd932942278ad83763ae6375c7
Arnaldo Valente <gleam2003@msn.com>
2024-10-19 21:38:38 +0200

    Merge armbian-audiosettings with audiosettings

--- scriptmodules/supplementary/audiosettings.sh
+++ scriptmodules/supplementary/audiosettings.sh
@@ -12,19 +12,30 @@
 rp_module_id="audiosettings"
 rp_module_desc="Configure audio settings"
 rp_module_section="config"
-rp_module_flags="!all rpi"
+rp_module_flags="!all rpi armbian"
 
 function depends_audiosettings() {
     if [[ "$md_mode" == "install" ]]; then
-        getDepends alsa-utils
+        local depends=(alsa-utils)
+        isPlatform "armbian" && depends+=(pulseaudio pamixer)
+        getDepends "${depends[@]}"
     fi
 }
 
 function gui_audiosettings() {
     # Check if the internal audio is enabled
     if [[ `aplay -ql | grep -e bcm2835 -e vc4hdmi | wc -l` < 1 ]]; then
-        printMsgs "dialog" "On-board audio disabled or not present"
-        return
+        if [[ `aplay -ql | wc -l` < 1 ]]; then
+            printMsgs "dialog" "On-board audio disabled or not present"
+            return
+        else
+            if _pa_cmd_audiosettings systemctl -q --user is-enabled pulseaudio.service; then
+                _pulseaudio_armbian_audiosettings
+            else
+                _alsa_internal_armbian_audiosettings
+            fi
+            return
+        fi
     fi
 
     # The list of ALSA cards/devices depends on the 'snd-bcm2385' module parameter 'enable_compat_alsa'
@@ -37,6 +48,192 @@ function gui_audiosettings() {
         _bcm2835_alsa_compat_audiosettings
     else
         _bcm2835_alsa_internal_audiosettings
+    fi
+}
+
+function _alsa_internal_armbian_audiosettings() {
+    local cmd=(dialog --backtitle "$__backtitle" --menu "Set audio output (ALSA)" 22 86 16)
+    local options=()
+    local card_index
+    local card_label
+
+    # Get the list of Pi internal cards
+    while read card_no card_label; do
+        options+=("$card_no" "$card_label")
+    done < <(aplay -ql | sed -En -e '/^card/ {s/^card ([0-9]+): [^[]* \[([^]]*)\].*/\1 \2/; p}')
+    options+=(
+        M "Mixer - adjust output volume"
+        R "Reset to default"
+    )
+
+    choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+    if [[ -n "$choice" ]]; then
+        case "$choice" in
+            [0-9])
+                _asoundrc_save_armbian_audiosettings $choice ${options[$((choice*2+1))]}
+                printMsgs "dialog" "Set audio output to ${options[$((choice*2+1))]}"
+                ;;
+            M)
+                alsamixer >/dev/tty </dev/tty
+                alsactl store
+                ;;
+            R)
+                _reset_alsa-audiosettings
+                ;;
+        esac
+    fi
+}
+
+# configure the default ALSA soundcard based on chosen card index and type
+function _asoundrc_save_armbian_audiosettings() {
+    [[ -z "$1" ]] && return
+
+    local card_index=$1
+    local card_type=$2
+    local tmpfile="$(mktemp)"
+
+    if isPlatform "kms" && ! isPlatform "dispmanx" && [[ $card_type == "HDMI"* ]] || [[ $card_type == "hdmi"* ]]; then
+        # when the 'vc4hdmi' driver is used instead of 'bcm2835_audio' for HDMI,
+        # the 'hdmi:vchdmi[-idx]' PCM should be used for converting to the native IEC958 codec
+        # adds a volume control since the default configured mixer doesn't work
+        # (default configuration is at /usr/share/alsa/cards/vc4-hdmi.conf)
+        local card_name="$(cat /proc/asound/card${card_index}/id)"
+        cat << EOF > "$tmpfile"
+
+pcm.hdmi${card_index} {
+  type asym
+  playback.pcm {
+    type plug
+    slave.pcm "hw:$card_index,0"
+  }
+}
+
+ctl.!default {
+  type hw
+  card $card_index
+}
+
+pcm.softvolume {
+  type softvol
+  slave.pcm "dmixer"    # Link to dmixer instead of direct HDMI
+  control.name "HDMI Playback Volume"
+  control.card ${card_index}
+}
+
+pcm.softmute {
+  type softvol
+  slave.pcm "softvolume"
+  control.name "HDMI Playback Switch"
+  control.card ${card_index}
+  resolution 2
+}
+
+pcm.!default {
+  type plug
+  slave.pcm "softmute"
+}
+
+pcm.dmixer {
+  type dmix
+  ipc_key 1024
+  slave {
+    pcm "hw:$card_index,0"
+    channels 2
+    period_time 0
+    period_size 1024
+    buffer_size 4096
+    rate 44100
+  }
+  bindings {
+    0 0
+    1 1
+  }
+}
+EOF
+    else
+    cat << EOF > "$tmpfile"
+pcm.!default {
+  type asym
+  playback.pcm {
+    type plug
+    slave.pcm "output"
+  }
+}
+pcm.output {
+  type hw
+  card $card_index
+}
+ctl.!default {
+  type hw
+  card $card_index
+}
+EOF
+    fi
+    local dest="$home/.asoundrc"
+    mv "$tmpfile" "$dest"
+    chmod 644 "$dest"
+    chown $user:$user "$dest"
+}
+
+function _pulseaudio_armbian_audiosettings() {
+    local options=()
+    local sinks=()
+    local sink_index
+    local sink_label
+    local sound_server="PulseAudio"
+
+    # Check if PulseAudio is running, otherwise 'pactl' will not work
+    if ! _pa_cmd_audiosettings pactl info >/dev/null; then
+        printMsgs "dialog" "PulseAudio is present, but not running.\nAudio settings cannot be set right now."
+        return
+    fi
+    while read sink_index sink_label sink_id; do
+        options+=("$sink_index" "$sink_label")
+        sinks[$sink_index]=$sink_id
+    done < <(_pa_cmd_audiosettings pactl list sinks | \
+            awk -F [:=#] 'BEGIN {idx=0} /Sink/ {
+                             ctl_index=$2
+                             do {getline} while($0 !~ /card.name/ && $0 !~ /Formats/);
+                             if ( $2 != "" ) {
+                                gsub(/"|bcm2835[^a-zA-Z]+/, "", $2); # strip bcm2835 suffix on analog output
+                                gsub(/vc4[-]?/ , "", $2); # strip the vc4 suffix on HDMI output(s)
+                                if ( $2 ~ /hdmi/ ) $2=toupper($2)
+                                print idx,$2,ctl_index
+                                idx++
+                             }
+                         }'
+            )
+    _pa_cmd_audiosettings pactl info | grep -i pipewire >/dev/null && sound_server="PipeWire"
+    local cmd=(dialog --backtitle "$__backtitle" --menu "Set audio output ($sound_server)" 22 86 16)
+    options+=(
+        M "Mixer - adjust output volume"
+        R "Reset to default"
+        P "Disable $sound_server"
+    )
+    choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+    if [[ -n "$choice" ]]; then
+        case "$choice" in
+            [0-9]*)
+                _pa_cmd_audiosettings pactl set-default-sink ${sinks[$choice]}
+                rm -f "/etc/alsa/conf.d/99-retropie.conf"
+
+                printMsgs "dialog" "Set audio output to ${options[$((choice*2+1))]}"
+                ;;
+            M)
+                _pa_cmd_audiosettings alsamixer >/dev/tty </dev/tty
+                alsactl store
+                ;;
+            R)
+                rm -fr "$home/.config/pulse"
+                /etc/init.d/alsa-utils reset
+                alsactl store
+                printMsgs "dialog" "Audio settings reset to defaults"
+                ;;
+            P)
+                _toggle_${sound_server,,}_audiosettings "off"
+                printMsgs "dialog" "${sound_server} disabled"
+                ;;
+        esac
     fi
 }
 
